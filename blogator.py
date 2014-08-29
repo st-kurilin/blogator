11319a5ee5cf9b858e6b66a3bfd4da5dea4137ef
stanislav-kurlin <sku@tradeshift.com>
2014-08-29 21:07:59 +0200

    Clean up.

--- blogator.py
+++ blogator.py
@@ -1,38 +1,38 @@
 
 import sys
 
-
-def writeTemplated(template_path, out_path, data):
+def write_templated(template_path, out_path, data):
 	import pystache
 	outs = out_path.open('w')
 	template = pystache.parse(template_path.open('r').read())
 	renderer = pystache.Renderer()
 	outs.write(renderer.render(template, data))
 
+def generate_content(inp):
+	import markdown
+	return markdown.markdown(inp)
 
 def generate(templates_dir, posts_dir, out_dir):
-	import markdown
-	import pystache
-	from pathlib import Path
 	posts = []
-	for post_file in Path(posts_dir).iterdir():
+	for post_file in posts_dir.iterdir():
 		with post_file.open() as fin:
 			post = {'title' : post_file.with_suffix("._").name,
 				'link' 	: "./" + post_file.with_suffix(".html").name,
-				'content': markdown.markdown(fin.read())}
+				'content': generate_content(fin.read())}
 			posts.append(post)
-			writeTemplated(Path(templates_dir) / "post.template.html", Path(out_dir) / post_file.with_suffix(".html").name, post)			
-	writeTemplated(Path(templates_dir) / "index.template.html", Path(out_dir) / "index.html", {'posts':posts})
+			write_templated(templates_dir / "post.template.html", out_dir / post_file.with_suffix(".html").name, post)			
+	write_templated(templates_dir / "index.template.html", out_dir / "index.html", {'posts':posts})
 		
 def clean_dir(dir):
 	print ("cleaning")
 
-templatesDir = sys.argv[3]
-fromDir = sys.argv[1]
-toDir = sys.argv[2]
+if __name__ == "__main__":
+	from pathlib import Path
+	from_dir = Path(sys.argv[1])
+	to_dir = Path(sys.argv[2])
+	templates_dir = Path(sys.argv[3])
 
-
-generate(templatesDir, fromDir, toDir)
+	generate(templates_dir, from_dir, to_dir)
 
 
 
